37811ba5d48faf46ec34ba71853fc07a9ea90130
gutao <1093656744@qq.com>
2015-06-19 23:59:33 +0800

    gstart

---
+++ src/gt/shop/pageModel/Pageproduct.java
@@ -0,0 +1,55 @@
+package gt.shop.pageModel;
+
+public class Pageproduct {
+     
+	 private String id;
+     private String url;
+     private String name;
+     private String description;
+     private String shop;
+     private String shopaddress;
+     private double price;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getShop() {
+		return shop;
+	}
+	public void setShop(String shop) {
+		this.shop = shop;
+	}
+	public String getShopaddress() {
+		return shopaddress;
+	}
+	public void setShopaddress(String shopaddress) {
+		this.shopaddress = shopaddress;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+     
+}
